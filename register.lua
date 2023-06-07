e90a0f6d64c9384de6ee34981a4f24099c04ef3f
maxchen32 <65599741+maxchen32@users.noreply.github.com>
2023-06-07 22:54:39 +0800

    fixproblem in node name, add chinese translation  (#6)
    * Changed the names of moreblocks nodes
    
    * add Chinese translation
    
    * code style
    ---------
    
    Co-authored-by: maxchen32 <max_chen32@foxmail.com>
    Co-authored-by: Niklp <89982526+Niklp09@users.noreply.github.com>

--- register.lua
+++ register.lua
@@ -44,12 +44,15 @@ for _, concrete in pairs(concrete) do
     end
 
     if moreblocks then
-        stairsplus:register_all("moreblocks", "colored_concrete:" .. concrete[1], "colored_concrete:" .. concrete[1], {
+        stairsplus:register_all("moreblocks", "colored_concrete_" .. concrete[1], "colored_concrete:" .. concrete[1], {
             description = S(concrete[2] .. " Concrete"),
             tiles = {"colored_concrete_" .. concrete[1] .. ".png"},
             groups = {cracky = 3},
             sounds = default.node_sound_stone_defaults(),
         })
+        if minetest.settings:get_bool("colored_concrete_enable_aliases", "true") then
+            stairsplus:register_alias_all("moreblocks", "colored_concrete:" .. concrete[1], "moreblocks", "colored_concrete_" .. concrete[1])
+        end
     end
 end
 
